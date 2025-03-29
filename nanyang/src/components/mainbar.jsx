a734fad53f73d7a6b8eb14cdc2591393ae520f33
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-03-29 19:35:50 +1100

    Layout in Mainbar

--- nanyang/src/components/mainbar.jsx
+++ nanyang/src/components/mainbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NTUSG } from '../assets/NTU_Logo.png'
+import { NTU60Year } from '../assets/sg60_ntusg.svg'
 
 function Mainbar() {
   return (
-    <div className='w-full h-1/5 flex font-serif p-8'>
-      <div className='flex-col border-2 border-black border-solid'>
-        <img src={ NTUSG } className='' />
-        <ul>
+    <div className='w-full h-1/5 grid grid-rows-4 grid-cols-6 font-serif p-5 fixed'>
+      <div className='flex flex-col col-span-1 border-2 border-black border-solid bg-white'>
+        <img src={ NTUSG } alt='NTU' className='' />
+        <ul className='space-x-2'>
           <li>About Us</li>
           <li>Admission</li>
           <li>Education</li>
@@ -14,8 +15,11 @@ function Mainbar() {
           <li>Innovation</li>
           <li>Life at NTU</li>
         </ul>
+        <div>
+          <img src={ NTU60Year } alt='60yearNTU' className='' />
+        </div>
       </div>
-      <div className='flex-col border-2 border-black border-solid'>
+      <div className=''>
 
       </div>
     </div>
