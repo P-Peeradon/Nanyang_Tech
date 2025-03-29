e1c6ad97248607418dff9df9525121645794ad5a
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-03-29 19:27:19 +1100

    Menu in mainbar

--- nanyang/src/components/mainbar.jsx
+++ nanyang/src/components/mainbar.jsx
@@ -3,19 +3,22 @@ import { NTUSG } from '../assets/NTU_Logo.png'
 
 function Mainbar() {
   return (
-    <>
-      <nav className='w-full h-1/5 flex '>
-        <div className='flex-col'>
-          <img src={ NTUSG } className='' />
-          <ul>
-            <li>Nanyang</li>
-          </ul>
-        </div>
-        <div className=''>
+    <div className='w-full h-1/5 flex font-serif p-8'>
+      <div className='flex-col border-2 border-black border-solid'>
+        <img src={ NTUSG } className='' />
+        <ul>
+          <li>About Us</li>
+          <li>Admission</li>
+          <li>Education</li>
+          <li>Research</li>
+          <li>Innovation</li>
+          <li>Life at NTU</li>
+        </ul>
+      </div>
+      <div className='flex-col border-2 border-black border-solid'>
 
-        </div>
-      </nav>
-    </>
+      </div>
+    </div>
   );
 }
 
