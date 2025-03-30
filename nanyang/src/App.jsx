f8039d0c9536da0f24ba517fbff942d222f332be
P-Peeradon <p.determinant.integrate3@gmail.com>
2025-03-30 13:10:35 +1100

    Fix Homepage

--- nanyang/src/App.jsx
+++ nanyang/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Mainbar from './components/mainbar';
+
 
 function App() {
   return (
     <>
-      <Mainbar />
+      
     </>
   );
 }
--- nanyang/src/components/mainbar.jsx
+++ nanyang/src/components/mainbar.jsx
@@ -1,29 +1,18 @@
-import React from 'react'
-import { NTUSG } from '../assets/NTU_Logo.png'
-import { NTU60Year } from '../assets/sg60_ntusg.svg'
+import NTU from '../assets/NTU_Logo.png'
 
 function Mainbar() {
   return (
-    <div className='w-full h-1/5 grid grid-rows-4 grid-cols-6 font-serif p-5 fixed'>
-      <div className='flex flex-col col-span-1 border-2 border-black border-solid bg-white'>
-        <img src={ NTUSG } alt='NTU' className='' />
-        <ul className='space-x-2'>
-          <li>About Us</li>
-          <li>Admission</li>
-          <li>Education</li>
-          <li>Research</li>
-          <li>Innovation</li>
-          <li>Life at NTU</li>
-        </ul>
+    <nav className="w-full h-1/5 flex flex-row bg-white ">
+      <div className="">
         <div>
-          <img src={ NTU60Year } alt='60yearNTU' className='' />
+          <img src={NTU} alt='NTUSG' width={200} height={150} />
         </div>
+        <li className=''></li>
       </div>
-      <div className=''>
-
-      </div>
-    </div>
-  );
+      <div></div>
+    </nav>
+  )
 }
 
 export default Mainbar;
+
